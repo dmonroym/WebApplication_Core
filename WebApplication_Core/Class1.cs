da49c796995a1bae40c5f89ca6f76c71635b4a9a
Daniel Monroy <dmon@microsoft.com>
2020-02-13 13:20:02 -0800

    initial commit

---
+++ WebApplication_Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication_Core
+{
+    internal class Class1
+    {
+    }
+}
---
+++ WebApplication_Core/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Core
+{
+    public class Class2
+    {
+        Class1 c = new Class1();
+    }
+}
